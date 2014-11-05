e03da883e25c466d5e385bf1b2983f05a1b23da5
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-11-05 14:58:38 -0500

    pictures have their own dynamic urls now.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,5 +19,26 @@ class PicturesController < ApplicationController
       }
     ]
   
+    def show
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
+  end
+
 	end
 end
